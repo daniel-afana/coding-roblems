4e18dc86110021e9040682fab00281594bb3b86b
Данил Афанасьев <d.afanasev@bars-open.ru>
2020-05-07 00:01:25 +0300

    Add funtion which reverses a sequence

---
+++ reverse-iterable.py
@@ -0,0 +1,7 @@
+def reversed_iterable(s):
+    u"""Reverses a sequence using a built-in function
+    :param s: iterable with a  __reversed__() method or
+    _getitem__() and __len__() methods
+    :return: reversed sequence
+    """
+    return reversed(s)
